818c6be6bf2c777bd679410f5e220ea6e8a8d2f8
Jano Alimenti Bel <janoalimentibel@MacBook-Pro.local>
2026-07-29 13:18:24 +0200

    Actualiza identificador visible a v3.19

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -105,7 +105,7 @@ export default function App() {
           <div className="streak">
             <div className="n">{savedCount}</div>
             <div className="l">sesiones</div>
-            <div className="version">v3.18</div>
+            <div className="version">v3.19</div>
           </div>
         </div>
       </div>
